38399e43c0799894ef29828639fe38df32397768
AhmedAbdoOrtiga <45858632+AhmedAbdoOrtiga@users.noreply.github.com>
2019-05-01 16:46:12 -0700

    Rename Folders to Axe.Windows.* instead of AccessibilityInsights.* (#17)
    - Rename Folders to Axe.Windows.* instead of AccessibilityInsights.*
    - Replace "AccessibilityInsights" with "Axe.Windows" in all files in the repo:
      - .sln file.
      - .csproj files.
      - yml files.
      - .cs files like AssemblyInfo.cs.
      - .ps1 files like the automations checks.
      - .nuspec files.
      - .fakes files.
    - Remove compliance yml file since it's not used anymore.
    - Remove Axe.Windows prefix from Telemetry project.
    - Alpha sort file paths in the signed build yml files.
    - Remove check installed automation and release cleanup scripts since they're meant for AI-Win.

---
+++ src/Axe.Windows.RuleSelection/Interfaces/IReferenceLinks.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Axe.Windows.Extensions.Interfaces.ReferenceLinks
+{
+    /// <summary>
+    /// Provides links to reference documentation
+    /// which the Axe.Windows rules are meant to validate.
+    /// </summary>
+    public interface IReferenceLinks
+    {
+        IReferenceLink GetReferenceLink(string lookupToken);
+    } // class
+} // namespace
