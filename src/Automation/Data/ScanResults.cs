59cad08df56f0f17c6054eee18b7e7b1fb6ad90b
DaveTryon <Dave.Tryon@microsoft.com>
2019-07-02 15:31:54 -0700

    Add cref to OutputFile

--- src/Automation/Data/ScanResults.cs
+++ src/Automation/Data/ScanResults.cs
@@ -10,7 +10,7 @@ namespace Axe.Windows.Automation
     public class ScanResults
     {
         /// <summary>
-        /// An OutputFile object containing paths to any output files written as
+        /// An OutputFile object <see cref="OutputFile"/> containing paths to any output files written as
         /// a result of a scan.
         /// </summary>
         /// <remarks>
