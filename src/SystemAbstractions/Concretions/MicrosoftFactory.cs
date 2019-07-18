5b372e2daace8bff927bb417f130a28dac655a82
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-07-18 11:42:44 -0400

    Convert Win32 project to .Net Standard
    - Have new project use .Net Standard equivalent NuGet packages for System.Drawing and Microsoft.Win32.Registry
    - Enable Win32 project unit testing without Fakes
    	- Add Microsoft.Win32.Registry (.Net Standard compatible) to SystemAbstractions
    	- Make Win32Helper an object
    	- Make Win32Helper use SystemAbstractions to access the registry
    	- 	Replace Fakes with mocks in Win32Tests project
    
    Note: Action item to organize SystemAbstractions in a more conventional way

---
+++ src/SystemAbstractions/Concretions/MicrosoftFactory.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace Axe.Windows.SystemAbstractions
+{
+    public class MicrosoftFactory : IMicrosoftFactory
+    {
+        private readonly static IMicrosoftFactory MicrosoftFactoryInstance = new MicrosoftFactory();
+
+        private MicrosoftFactory()
+        { }
+
+        public static IMicrosoft CreateMicrosoft()
+        {
+            return new Microsoft(MicrosoftFactoryInstance);
+        }
+
+        public IMicrosoftWin32 CreateMicrosoftWin32()
+        {
+            return new MicrosoftWin32(MicrosoftFactoryInstance);
+        }
+
+        public IMicrosoftWin32Registry CreateMicrosoftWin32Registry()
+        {
+            return new MicrosoftWin32Registry();
+        }
+    } // class
+} // namespace
---
+++ src/SystemAbstractions/Concretions/MicrosoftWin32Registry.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Microsoft.Win32;
+using System;
+
+namespace Axe.Windows.SystemAbstractions
+{
+    class MicrosoftWin32Registry : IMicrosoftWin32Registry
+    {
+        public object GetValue(string keyName, string valueName, object defaultValue)
+        {
+            return Registry.GetValue(keyName, valueName, defaultValue);
+        }
+    } // class
+} // namespace
