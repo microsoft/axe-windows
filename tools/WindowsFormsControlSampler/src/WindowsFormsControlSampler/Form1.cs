10fc4915c8bf5af57615cd1f444bec8d0080a8c3
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2019-09-25 09:54:40 -0700

    Add control sampler apps for testing (#155)

---
+++ tools/WindowsFormsControlSampler/src/WindowsFormsControlSampler/Form1.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsControlSampler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
