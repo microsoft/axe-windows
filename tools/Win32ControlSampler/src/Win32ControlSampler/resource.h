8a969bc05c9c3f7c3a7f86c81b600bf9a7786af3
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2019-09-27 10:48:43 -0700

    Add a simple Win32 control sampler app (#156)

---
+++ tools/Win32ControlSampler/src/Win32ControlSampler/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Win32ControlSampler.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_SAMPLER                     103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT1             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDC_CHECK1                      110
+#define IDC_COMBO1                      111
+#define IDC_EDIT1                       112
+#define IDC_LIST1                       113
+#define IDC_WIN32CONTROLSAMPLER         114
+#define IDR_MAINFRAME                   128
+#define IDC_RADIO1                      1000
+#define IDC_RADIO2                      1001
+#define IDC_RICHEDIT21                  1002
+#define IDC_SLIDER1                     1003
+#define IDC_TAB1                        1004
+#define IDC_DATETIMEPICKER1             1005
+#define IDC_MONTHCALENDAR1              1006
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           115
+#endif
+#endif
