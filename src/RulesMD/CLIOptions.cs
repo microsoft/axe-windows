f01d5fdd89ed3162758fe0ee9c86129ff81e3d86
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-10-21 15:59:10 -0400

    Add RulesMD project for generating markdown from rules info
    - Add target to run RulesMD after build

---
+++ src/RulesMD/CLIOptions.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using CommandLine;
+using System;
+
+namespace RulesMD
+{
+    class CLIOptions
+    {
+        [Option('o', "output", Required = true,
+            HelpText = "path to the markdown file to be created")]
+        public string OutputPath { get; set; }
+    } // class
+} // namespace
---
+++ src/RulesMD/Helpers.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Rules;
+using System;
+
+namespace RulesMD
+{
+    static class Helpers
+    {
+        public static string GetStandardName(A11yCriteriaId criteriaId)
+        {
+            switch (criteriaId)
+            {
+                case A11yCriteriaId.AvailableActions:
+                    return "Section 508 502.3.10";
+                case A11yCriteriaId.InfoAndRelationships:
+                    return "WCAG 1.3.1";
+                case A11yCriteriaId.Keyboard:
+                    return "WCAG 2.1.1";
+                case A11yCriteriaId.NameRoleValue:
+                    return "WCAG 4.1.2";
+                case A11yCriteriaId.ObjectInformation:
+                    return "Section 508 502.3.1";
+            }
+
+            return "none";
+        }
+    } // class
+} // namespace
