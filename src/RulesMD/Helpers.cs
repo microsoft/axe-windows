78314ef04c023b9fe0a1cefd33df4c9796f4e7f3
RobGallo <43080011+RobGallo@users.noreply.github.com>
2019-10-25 08:40:23 -0400

    Add RulesMD project for generating markdown from rules info (#177)
    * Add RulesMD project for generating markdown from rules info
    
    - Add target to run RulesMD after build
    
    * Add license header to a source file
    
    Also, delete generated file.
    
    * Update proj file to generate text template output before building
    
    * Revert solution to use VS 2017
    
    Changed unintentionally
    
    * Fix solution version
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Fix indent
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Fix indent
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Fix indent
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Fix Indent
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Fix indent
    
    Co-Authored-By: DaveTryon <45672944+DaveTryon@users.noreply.github.com>
    
    * Update RulesMD code to use string resources from RulesSelector rather than have a separate set of hard-coded strings

---
+++ src/RulesMD/Helpers.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Rules;
+using Axe.Windows.RuleSelection.Resources;
+using System;
+
+namespace RulesMD
+{
+    static class Helpers
+    {
+        public static string GetStandardName(A11yCriteriaId criteriaId)
+        {
+            switch (criteriaId)
+            {
+                case A11yCriteriaId.AvailableActions:
+                    return DefaultGuidelineShortDescriptions.AvailableActions;
+                case A11yCriteriaId.InfoAndRelationships:
+                    return DefaultGuidelineShortDescriptions.InfoAndRelationships;
+                case A11yCriteriaId.Keyboard:
+                    return DefaultGuidelineShortDescriptions.Keyboard;
+                case A11yCriteriaId.NameRoleValue:
+                    return DefaultGuidelineShortDescriptions.NameRoleValue;
+                case A11yCriteriaId.ObjectInformation:
+                    return DefaultGuidelineShortDescriptions.ObjectInformation;
+            }
+
+            return DefaultGuidelineShortDescriptions.None;
+        }
+    } // class
+} // namespace
---
+++ src/RulesMD/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RulesMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoWork(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(-1);
+            }
+        }
+
+        static void DoWork(string[] args)
+        {
+            Parser.Default.ParseArguments < CLIOptions>(args)
+                .WithParsed<CLIOptions>(Run);
+        }
+
+        private static void Run(CLIOptions options)
+        {
+            var markdownCreator = new MarkdownCreator();
+            var markdown = markdownCreator.TransformText();
+
+            var f = File.CreateText(options.OutputPath);
+            f.Write(markdown);
+            f.Close();
+        }
+    } // class
+} // namespace
