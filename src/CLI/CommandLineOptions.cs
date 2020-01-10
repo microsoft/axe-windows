a9bc16cae22df19ee1bfa0738cd0fcc7898402a4
Dave Tryon <dtryon@microsoft.com>
2020-01-10 10:18:54 -0800

    Add CLI, early command line processor

---
+++ src/CLI/CommandLineOptions.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AxeWindowsScanner
+{
+    public class CommandLineOptions
+    {
+        [Option('d', "directory", Required = false, HelpText = "Output file")]
+        public string Directory { get; set; }
+
+        [Option('f', "file", Required = false, HelpText = "Output file")]
+        public string File { get; set; }
+
+        [Option('i', "id", Required = false, HelpText = "Process Id")]
+        public int Id { get; set; }
+
+        [Option('n', "name", Required = false, HelpText = "Process name")]
+        public string Name { get; set; }
+
+        [Option('v', "verbose", Required = false, HelpText = "Use verbose output")]
+        public bool Verbose { get; set; }
+
+        [Option('q', "quiet", Required = false, HelpText = "Use quiet output")]
+        public bool Quiet { get; set; }
+    }
+}
---
+++ src/CLI/Program.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AxeWindowsScanner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string[] t = { "-verbose", "-quiet", "-d", "abc", "-f", "def", "-i", "111", "-n", "excel.exe" };
+			var result = Parser.Default.ParseArguments<CommandLineOptions>(t).MapResult((opts) => RunOptionsAndReturnExitCode(opts), //in case parser sucess
+				errs => HandleParseError(errs)); //in case parser fails
+			Console.WriteLine("Return code= {0}", result);
+			return -1;
+		}
+
+		static int RunOptionsAndReturnExitCode(CommandLineOptions o)
+		{
+			var exitCode = 0;
+
+			Console.WriteLine("Verbose = {0}", o.Verbose);
+			Console.WriteLine("Quiet = {0}", o.Quiet);
+			Console.WriteLine("OutputFile = {0}", o.File);
+			Console.WriteLine("Directory = {0}", o.Directory);
+			Console.WriteLine("Id = {0}", o.Id);
+			Console.WriteLine("Name = {0}", o.Name);
+
+			return exitCode;
+		}
+
+		//in case of errors or --help or --version
+		static int HandleParseError(IEnumerable<Error> errs)
+		{
+			var result = -2;
+			Console.WriteLine("errors {0}", errs.Count());
+			if (errs.Any(x => x is HelpRequestedError || x is VersionRequestedError))
+				result = -1;
+			Console.WriteLine("Exit code {0}", result);
+			return result;
+		}
+    }
+}
