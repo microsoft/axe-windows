4c7f469c2a5e6434324087a6a83f4e02c3d79e32
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-01-21 17:08:02 -0800

    Revert changes to the OldFileVersionCompatibilityTests project
    Some unit tests in this project were not running in .Net Core. The culprit was likely missing NuGet package references. Those have been added, but they have been added to the .Net Framework version of the project. This is because it turns out that it wasn't necessary to convert this project to .Net Core for this PR. And now that the unit tests compile, it doesn't seem worth while to change something that works and is not production code.

---
+++ src/OldFileVersionCompatibilityTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Axe.Windows.OldFileVersionCompatibilityTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Axe.Windows.OldFileVersionCompatibilityTests")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21f31760-d98d-41e6-bb7a-52619ed460c5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
