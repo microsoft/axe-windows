711ebf98d449dffbac3c560332cae74708cffd0a
DaveTryon <45672944+DaveTryon@users.noreply.github.com>
2020-01-31 12:29:57 -0800

    Reorder Options class to improve auto-generated help (#288)

--- src/CLI/Options.cs
+++ src/CLI/Options.cs
@@ -7,17 +7,17 @@ namespace AxeWindowsCLI
 {
     public class Options : IOptions
     {
-        [Option(Required = false, HelpText = "Output directory")]
-        public string OutputDirectory { get; set; }
-
-        [Option(Required = false, HelpText = "Scan ID")]
-        public string ScanId { get; set; }
-
         [Option(Group = "Target", Required = false, HelpText = "Process Id")]
         public int ProcessId { get; set; }
 
         [Option(Group = "Target", Required = false, HelpText = "Process Name")]
         public string ProcessName { get; set; }
+
+        [Option(Required = false, HelpText = "Output directory")]
+        public string OutputDirectory { get; set; }
+
+        [Option(Required = false, HelpText = "Scan ID")]
+        public string ScanId { get; set; }
 
         [Option(Required = false, HelpText = "Verbosity level (Quiet/Default/Verbose)")]
         public string Verbosity { get; set; }
