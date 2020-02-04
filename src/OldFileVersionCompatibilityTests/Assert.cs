743215c3586b75b9f02dc0412a234d69ed7298f2
RobGallo <43080011+RobGallo@users.noreply.github.com>
2020-02-04 07:08:15 -0800

    Add unit tests to check old a11ytest file version compatibility (#291)
    Changing the OldFileVersionCompatibilityTests project so that it is a console app rather than a test app.
    
    The problem was that since we changed the project to .Net Core, running all three unit tests which checked for old file version compatibility caused all three tests to appear not to run. The investigation into the problem determined there was a missing dll: Microsoft.IntelliTrace.Core. The trail went cold at that point: there is no documentation on this problem except that it exists and that it should have been fixed in the latest version of Visual Studio and .Net Core, although there are examples of users who still experience the problem with the latest versions just like us.
    
    Since the tests run and pass individually, the solution (implemented herein) is to run each test by calling a console app and specifying the particular test function to run.
    
    It isn't pretty, but there it is.

---
+++ src/OldFileVersionCompatibilityTests/Assert.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using System;
+
+namespace OldFileVersionCompatibilityTests
+{
+    static class Assert
+    {
+        public static void AreEqual(object o1, object o2)
+        {
+            if (!o1.Equals(o2))
+                throw new TestAssertionException($"Expected {o1} to be equal to {o2}");
+        }
+
+        public static void AreNotEqual(object o1, object o2)
+        {
+            if (o1.Equals(o2))
+                throw new TestAssertionException($"Expected {o1} not to be equal to {o2}");
+        }
+
+        public static void IsFalse(bool b)
+        {
+            if (b)
+                throw new TestAssertionException($"Expected: false, actual: true");
+        }
+
+        public static void IsNotNull(object o)
+        {
+            if (o == null)
+                throw new TestAssertionException($"Expected the given object not to be null");
+        }
+    } // class
+} // namespace
