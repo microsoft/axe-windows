0133a48b69feb798552af7324c965295f6b39f7a
Dave Tryon <dtryon@microsoft.com>
2020-02-17 13:57:34 -0800

    Create a ZIP file of the CLI_Full project, with the signed binarires from CLI

--- tools/scripts/BuildZippedCLI.ps1
+++ tools/scripts/BuildZippedCLI.ps1
@@ -8,16 +8,24 @@ Builds a ZIP file that contains the Axe.Windows CLI. Output is named AxeWindowsC
 .PARAMETER Configuration
 Debug or Release. Will retain PDB files in the ZIP file if DEBUG. Use $(ConfigurationName) if calling from a csproj file
 
-.PARAMETER TargetDir
-The directory where the files were built (and where the ZIP file will be dropped). Use $(TargetDir) if calling from a csproj file
+.PARAMETER FullTargetDir
+The directory where the full files were built.
+
+.PARAMETER SignedTargetDir
+The directory where the signed CLI files were built.
+
+.PARAMETER OutputDir
+The directory where the .zip file will be dropped. Don't put this under FullTargetDir or the zip file will be "nested" in subsequent runs
 
 .Example Usage
-.\BuildZippedCLI.ps1 -Configuration release -TargetDir c:\myrepo\src\CLI\bin\release\netcoreapp3.0
+.\BuildZippedCLI.ps1 -Configuration release -FullTargetDir c:\myrepo\src\CLI_Full\bin\release\netcoreapp3.0\win7-x86 -SignedTargetDir c:\myrepo\src\CLI\bin\release\netcoreapp3.0 -OutputDir c:\myrepo\src\CLI_Installer\bin\release
 #>
 
 param(
-    [Parameter(Mandatory=$true)][String]$Configuration,
-    [Parameter(Mandatory=$true)][String]$TargetDir
+    [Parameter(Mandatory=$true)][string]$Configuration,
+    [Parameter(Mandatory=$true)][string]$FullTargetDir,
+    [Parameter(Mandatory=$true)][string]$SignedTargetDir,
+    [Parameter(Mandatory=$true)][string]$OutputDir
 )
 
 Set-StrictMode -Version Latest
@@ -29,64 +37,57 @@ $script:ErrorActionPreference = 'Stop'
 # This suppresses the progress indicator while creating the zip file
 $ProgressPreference='SilentlyContinue'
 
-function Get-UniqueTempFolder($app){
+function Get-UniqueTempFolder([string] $app){
     $guid=New-Guid
     $uniqueName=$app + '_' + $guid
     $tempFolder=Join-Path $env:temp $uniqueName
+	New-Item -Path $tempFolder -ItemType Directory
     return $tempFolder
 }
 
-function Copy-SourceTree($src, $dst){
-    Copy-Item $src $dst -force -recurse >$null
+function Copy-Files([string]$src, [string]$pattern, [string]$dst) {
+    [string]$srcWithPattern = Join-Path $src $pattern
+    Write-Verbose "Copying $srcWithPattern to $dst"
+
+    Copy-Item $srcWithPattern $dst -Force
 }
 
-function Remove-FilesByExtension($path, $extension){
-    $pattern='*.' + $extension
+function Remove-FilesByPattern([string]$path, [string]$pattern){
     Get-ChildItem $path -File -Recurse -Include $pattern | Remove-Item
 }
 
-function Remove-FoldersByName($path, $name){
-    Get-ChildItem $path -Directory -Recurse -Include $name | Remove-Item -Force -Recurse
-}
-
-function Create-Archive($src, $dst){
+function Create-Archive([string]$src, [string]$dst){
     $from=$src + '\*'
     Compress-Archive -Force -Path $from -DestinationPath $dst
 }
 
-function Create-Zipfile($src, $app, $zipFile, [string[]]$extensionsToPrune, [string[]]$foldersToPrune){
-    Write-Verbose "src = $src"
+function Create-Zipfile([string]$fullTargetSrc, [string]$signedTargetSrc, [string]$app, [string]$zipFile, [string[]]$patternsToPrune){
+    Write-Verbose "fullTargetSrc = $fullTargetSrc"
+    Write-Verbose "signedTargetSrc = $signedTargetSrc"
     Write-Verbose "app = $app"
     Write-Verbose "zipFile = $zipFile"
-    Write-Verbose "extensionsToPrune = $extensionsToPrune"
-    Write-Verbose "foldersToPrune = $foldersToPrune"
+    Write-Verbose "patternsToPrune = $patternsToPrune"
 
-    $scratch=Get-UniqueTempFolder $app
-    Write-Verbose "$scratch = $scratch"
-    Copy-SourceTree $src $scratch
-    foreach ($extension in $extensionsToPrune) {
-        Remove-FilesByExtension $scratch $extension
-    }
-    foreach ($folder in $foldersToPrune) {
-        Remove-FoldersByName $scratch $folder
+    $scratch=(Get-UniqueTempFolder $app)[0]
+    Write-Verbose "scratch = $scratch"
+
+    Copy-Files $fullTargetSrc '*.*' $scratch
+    Copy-Files $signedTargetSrc ($app + '.*') $scratch
+    foreach ($pattern in $patternsToPrune) {
+        Remove-FilesByPattern $scratch $pattern
     }
     Create-Archive $scratch $zipFile
 
     Remove-Item $scratch -Force -Recurse
 }
 
-function CreateCLIZip($configuration, $targetDir, $zipFileName){
-    Write-Host "Creating a CLI zip file from files in $targetDir"
-    if ($configuration -eq 'debug') {
-        $extensionsToPrune=@('dev.json')
-    } else {
-        $extensionsToPrune=@('pdb', 'dev.json')
-    }
-    $foldersToPrune=@('unix')
-    $zipFile=Join-Path $targetDir $zipFileName
-    Create-ZipFile $targetDir 'CLI' $zipFile $extensionsToPrune $foldersToPrune
+function CreateCLIZip([string]$configuration, [string]$fullTargetDir, [string]$signedTargetDir, [string]$outputDir, [string]$zipFileName){
+    Write-Host "Creating an AxeWindowsCLI zip file from files in $fullTargetDir and $signedTargetDir"
+	$patternsToPrune=@('AxeWindowsCLIPlaceholder.*', '*.dev.json', '*.pdb')
+    $zipFile=Join-Path $outputDir $zipFileName
+    Create-ZipFile $fullTargetDir $signedTargetDir 'AxeWindowsCLI' $zipFile $patternsToPrune
     Write-Host 'Successfully Created' $zipFile
 }
 
-CreateCLIZip $Configuration $TargetDir 'AxeWindowsCLI.zip'
+CreateCLIZip $Configuration $FullTargetDir $SignedTargetDir $OutputDir 'AxeWindowsCLI.zip'
 exit 0
