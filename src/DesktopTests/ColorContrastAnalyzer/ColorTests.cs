3accd460ab1299946220f1984a1ea2ad8b9a78e3
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2020-09-24 11:27:42 -0700

    Add checks that color contrast is reflexive

--- src/DesktopTests/ColorContrastAnalyzer/ColorTests.cs
+++ src/DesktopTests/ColorContrastAnalyzer/ColorTests.cs
@@ -1,8 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Axe.Windows.Desktop.ColorContrastAnalyzer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Axe.Windows.Desktop.ColorContrastAnalyzer;
 
 namespace Axe.Windows.DesktopTests.ColorContrastAnalyzer
 {
@@ -10,7 +11,7 @@ namespace Axe.Windows.DesktopTests.ColorContrastAnalyzer
     public class ColorTests
     {
         [TestMethod, Timeout(2000)]
-        public void ColorContrastTest_BlackAndWhite()
+        public void ColorContrastTest_BlackAndWhite_CorrectAndReflexive()
         {
             Color black = new Color(0, 0, 0);
             Color white = new Color(255, 255, 255);
@@ -20,21 +21,24 @@ namespace Axe.Windows.DesktopTests.ColorContrastAnalyzer
         }
 
         [TestMethod, Timeout(2000)]
-        public void ColorContrastTest_WhiteAndRed()
+        public void ColorContrastTest_WhiteAndRed_CorrectAndReflexive()
         {
             Assert.AreEqual(4.00, Math.Round(Color.RED.Contrast(Color.WHITE), 2));
+            Assert.AreEqual(4.00, Math.Round(Color.WHITE.Contrast(Color.RED), 2));
         }
 
         [TestMethod, Timeout(2000)]
-        public void ColorContrastTest_RedAndBlue()
+        public void ColorContrastTest_RedAndBlue_CorrectAndReflexive()
         {
             Assert.AreEqual(2.15, Math.Round(Color.RED.Contrast(Color.BLUE), 2));
+            Assert.AreEqual(2.15, Math.Round(Color.BLUE.Contrast(Color.RED), 2));
         }
 
         [TestMethod, Timeout(2000)]
-        public void ColorContrastTest_RedAndGreen()
+        public void ColorContrastTest_RedAndGreen_CorrectAndReflexive()
         {
             Assert.AreEqual(2.91, Math.Round(Color.RED.Contrast(Color.GREEN), 2));
+            Assert.AreEqual(2.91, Math.Round(Color.GREEN.Contrast(Color.RED), 2));
         }
 
         [TestMethod, Timeout(2000)]
