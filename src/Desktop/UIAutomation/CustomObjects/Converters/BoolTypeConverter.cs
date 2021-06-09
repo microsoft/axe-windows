70faf412b89c8080cae1a8462c0c36f163cffdc8
Bill Dengler <codeofdusk@gmail.com>
2021-06-09 19:16:37 -0400

    Check-in type converters.

---
+++ src/Desktop/UIAutomation/CustomObjects/Converters/BoolTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Interop.UIAutomationCore;
+using System;
+using System.Globalization;
+
+namespace Axe.Windows.Desktop.UIAutomation.CustomObjects.Converters
+{
+    class BoolTypeConverter : ITypeConverter
+    {
+        public UIAutomationType UnderlyingUiaType => UIAutomationType.UIAutomationType_Bool;
+
+        public string Render(dynamic value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            return ((bool)value).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
