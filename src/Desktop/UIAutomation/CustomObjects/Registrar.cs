b33f8478c7bbe1e0efb4e3f000478c3c1e2ffd17
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-06-09 17:45:48 -0700

    demo: Consume interop from NuGet

---
+++ src/Desktop/UIAutomation/CustomObjects/Registrar.cs
@@ -0,0 +1,56 @@
+﻿using Interop.UIAutomationCore;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Axe.Windows.Desktop.UIAutomation.CustomObjects
+{
+    public class Registrar : IDisposable
+    {
+        CUIAutomationRegistrarClass _uiaRegistrar = new CUIAutomationRegistrarClass();
+        private bool disposedValue;
+
+        public int Test()
+        {
+            UIAutomationPropertyInfo info = new UIAutomationPropertyInfo
+            {
+                guid = Guid.Empty,
+                pProgrammaticName = "Some property",
+                type = UIAutomationType.UIAutomationType_Bool,
+            };
+
+            _uiaRegistrar.RegisterProperty(ref info, out int dynamicProperty);
+
+            return dynamicProperty;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // Free unmanaged resources (unmanaged objects) and set large fields to null
+                Marshal.ReleaseComObject(_uiaRegistrar);
+                _uiaRegistrar = null;
+
+                disposedValue = true;
+            }
+        }
+
+        ~Registrar()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
