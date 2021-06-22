6aad4d303ae5058f41962372dff8ff0692186d95
Bill Dengler <codeofdusk@gmail.com>
2021-06-21 21:29:45 -0400

    oopsy doopsy!)

--- src/CLI/ScanRunner.cs
+++ src/CLI/ScanRunner.cs
@@ -17,7 +17,6 @@ namespace AxeWindowsCLI
         {
             Config.Builder builder = Config.Builder
                 .ForProcessId(options.ProcessId)
-                .WithCustomUIAConfig(@"c:\excel.json")
                 .WithOutputFileFormat(OutputFileFormat.A11yTest);
             if (!string.IsNullOrEmpty(options.OutputDirectory))
                 builder = builder.WithOutputDirectory(options.OutputDirectory);
