d67c009177d3c7a69c644b82f9041e6a97b98ca5
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-07-01 11:40:27 -0700

    chore: Package Interop in dual formats (#611)
    #### Details
    
    We've tried packaging Interop.UIAutomationCore.dll as a purely embedded assembly and as a purely non-embedded assembly. Neither worked completely. This PR uses both strategies--it embeds the assembly into Axe.Windows.Desktop (for the NuGet package consumption) and carries along the external version as a build artifact to keep the CLI happy. This increases the CLI size slightly (about 50KB on disk), but solves all of our scenarios. We've confirmed that both the CLI and AIWin work correctly with this change.
    
    ##### Motivation
    
    Being able to release is important
    
    #### Pull request checklist
    - [n/a] Addresses an existing issue:

---
+++ src/Interop/ForcedReference.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Interop.UIAutomationCore;
+using System;
+
+namespace Axe.Windows.Interop
+{
+    /// <summary>
+    /// This class exists purely to provide a vehicle to allow the Desktop project to know
+    /// exactly where to find Interop.UIAutomationCore.dll for embedding.
+    /// </summary>
+    static public class ForcedReference
+    {
+        /// <summary>
+        /// This method never actually gets called by our code. It's here to force inclusion of
+        /// Interop.UIAutomationCore.dll.
+        /// </summary>
+        static public Type ForceReference()
+        {
+            var r = new CUIAutomationRegistrar();
+            return r.GetType();
+        }
+    }
+}
