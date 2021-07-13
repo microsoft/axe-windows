ab168f41a8ba0d91255768d3559e95a2936d3010
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-07-13 12:50:13 -0700

    feat(custom-uia): Fix problem with embedding Interop assembly (#616)

--- src/Interop/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Reflection;
-
-[assembly: AssemblyProduct("Axe.Windows.Interop")]
-[assembly: AssemblyTitle("Axe.Windows.Interop")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
---
+++ src/InteropDummy/Program.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Interop.UIAutomationCore;
+using System;
+
+namespace InteropDummy
+{
+    class Program
+    {
+        static int Main(string[] _)
+        {
+            var fakeType = new CUIAutomationRegistrar().GetType();
+
+#pragma warning disable CA1303 // Do not pass literals as localized parameters
+            Console.WriteLine("This program exists to import Interop.UIAutomationCore.Signed.dll");
+            Console.WriteLine("in a way that makes it easy to handle the licensing.");
+#pragma warning restore CA1303 // Do not pass literals as localized parameters
+
+            return (fakeType == typeof(int)) ? 1 : 0;
+        }
+    }
+}
