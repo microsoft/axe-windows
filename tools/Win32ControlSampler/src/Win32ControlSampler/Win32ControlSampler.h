bff2b0500225810fd4652d97651dcac791519313
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2021-11-17 16:08:09 -0800

    chore: Add SplitButton to win32 sampler app (#654)

--- tools/Win32ControlSampler/src/Win32ControlSampler/Win32ControlSampler.h
+++ tools/Win32ControlSampler/src/Win32ControlSampler/Win32ControlSampler.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #include "resource.h"
+
+// This is needed to load the appropriate version of comctl32.dll
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
--- tools/Win32ControlSampler/src/Win32ControlSampler/resource.h
+++ tools/Win32ControlSampler/src/Win32ControlSampler/resource.h
@@ -24,6 +24,7 @@
 #define IDC_TAB1                        1004
 #define IDC_DATETIMEPICKER1             1005
 #define IDC_MONTHCALENDAR1              1006
+#define IDC_SPLIT1                      1007
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -33,7 +34,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           115
 #endif
 #endif
