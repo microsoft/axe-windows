cfd6d9b8cbccf6508b19969df28f07d98c3e0143
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-02-01 09:51:14 -0800

    Fix test

--- src/DesktopTests/ColorContrastAnalyzer/ImageTests.cs
+++ src/DesktopTests/ColorContrastAnalyzer/ImageTests.cs
@@ -21,7 +21,7 @@ namespace Axe.Windows.DesktopTests.ColorContrastAnalyzer
             Assembly myAssembly = Assembly.GetExecutingAssembly();
             Stream myStream = myAssembly.GetManifestResourceStream("Axe.Windows.DesktopTests.TestImages." + name);
             Bitmap bmp = new Bitmap(myStream);
-            return new BitmapCollection(bmp, new DefaultColorContrastConfig());
+            return new BitmapCollection(bmp, new ColorContrastConfigBuilder().Build());
         }
 
         [TestMethod]
