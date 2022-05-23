f266593d7a530032183b0748b669c66246d2ae33
Katy DeCorah <katydecorah@microsoft.com>
2022-05-23 16:42:46 -0400

    feat(framework-issues-logic): Add rule `ControlShouldSupportTextPatternEditWinform`
    Co-Authored-By: SB <18401275+shanisebarona@users.noreply.github.com>

---
+++ src/Rules/Library/ControlShouldSupportTextPatternEditWinform.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+using Axe.Windows.Core.Bases;
+using Axe.Windows.Core.Enums;
+using Axe.Windows.Rules.PropertyConditions;
+using Axe.Windows.Rules.Resources;
+using System;
+using static Axe.Windows.Rules.PropertyConditions.ControlType;
+using static Axe.Windows.Rules.PropertyConditions.Framework;
+
+namespace Axe.Windows.Rules.Library
+{
+    [RuleInfo(ID = RuleId.ControlShouldSupportTextPatternEditWinform)]
+    class ControlShouldSupportTextPatternEditWinform : Rule
+    {
+        public ControlShouldSupportTextPatternEditWinform()
+        {
+            this.Info.Description = Descriptions.ControlShouldSupportTextPattern;
+            this.Info.HowToFix = HowToFix.ControlShouldSupportTextPattern;
+            this.Info.Standard = A11yCriteriaId.AvailableActions;
+            this.Info.ErrorCode = EvaluationCode.Error;
+            this.Info.FrameworkIssueLink = "https://aka.ms/FrameworkIssue-ControlShouldSupportTextPatternEditWinForm";
+        }
+
+        public override bool PassesTest(IA11yElement e)
+        {
+            if (e == null) throw new ArgumentNullException(nameof(e));
+
+            return Patterns.Text.Matches(e);
+        }
+
+        protected override Condition CreateCondition()
+        {
+            return Edit & WinForms;
+        }
+    } // class
+} // namespace
