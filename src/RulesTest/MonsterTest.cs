2a96c28a0c9c178c05df4fb83efa5bd2fe04a068
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-05-24 09:23:03 -0700

    Add new rule to test result

--- src/RulesTest/MonsterTest.cs
+++ src/RulesTest/MonsterTest.cs
@@ -103,6 +103,7 @@ namespace Axe.Windows.RulesTests
             Assert.AreEqual(EvaluationCode.Pass, results[RuleId.IsControlElementPropertyExists]);
             Assert.AreEqual(EvaluationCode.NotApplicable, results[RuleId.IsControlElementTrueOptional]);
             Assert.AreEqual(EvaluationCode.NotApplicable, results[RuleId.IsControlElementTrueRequired]);
+            Assert.AreEqual(EvaluationCode.Pass, results[RuleId.IsControlElementTrueRequiredButtonWPF]);
             Assert.AreEqual(EvaluationCode.NotApplicable, results[RuleId.IsKeyboardFocusableDescendantTextPattern]);
             Assert.AreEqual(EvaluationCode.NotApplicable, results[RuleId.IsKeyboardFocusableFalseButDisabled]);
             Assert.AreEqual(EvaluationCode.NotApplicable, results[RuleId.IsKeyboardFocusableFalseButOffscreen]);
