fe6b5d2d66246906eb4b948b48e59c76b0207a2b
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-08-19 16:20:29 -0700

    Add a suppression

--- src/Win32/Win32Constants.cs
+++ src/Win32/Win32Constants.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 namespace Axe.Windows.Win32
 {
@@ -10,6 +10,8 @@ namespace Axe.Windows.Win32
         public const int GWL_STYLE = -16;
         public const int GWL_EXSTYLE = -20;
 
+#pragma warning disable IDE1006 // Naming Styles
         public const int S_OK = 0;
+#pragma warning restore IDE1006 // Naming Styles
     }
 }
