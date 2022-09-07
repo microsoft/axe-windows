559a814f9515837a1c6555d06b1abb88bcb41621
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-09-07 14:30:01 -0700

    chore: Normalize resources for automation project

--- src/AutomationTests/OutputFileHelperTests.cs
+++ src/AutomationTests/OutputFileHelperTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using Axe.Windows.Automation;
+using Axe.Windows.Automation.Resources;
 using Axe.Windows.SystemAbstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
