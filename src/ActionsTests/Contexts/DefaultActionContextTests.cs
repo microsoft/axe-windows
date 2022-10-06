dfcd3c900fb1fe0e9a088e0f379bb4e1e2de6d2d
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-10-06 14:36:53 -0700

    Add unit tests for IActionContext implementations

---
+++ src/ActionsTests/Contexts/DefaultActionContextTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Axe.Windows.Actions;
+using Axe.Windows.Actions.Contexts;
+using Axe.Windows.Desktop.UIAutomation.CustomObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axe.Windows.ActionsTests.Contexts
+{
+    [TestClass]
+    public class DefaultActionContextTests
+    {
+        [TestMethod]
+        [Timeout(1000)]
+        public void DataManager_IsDefaultDataManager()
+        {
+            var dataManager = DefaultActionContext.GetDefaultInstance().DataManager;
+            Assert.IsNotNull(dataManager);
+            Assert.AreSame(DataManager.GetDefaultInstance(), dataManager);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void SelectAction_IsDefaultSelectAction()
+        {
+            var selectAction = DefaultActionContext.GetDefaultInstance().SelectAction;
+            Assert.IsNotNull(selectAction);
+            Assert.AreSame(SelectAction.GetDefaultInstance(), selectAction);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void Registrar_IsDefaultSelectAction()
+        {
+            var registrar = DefaultActionContext.GetDefaultInstance().Registrar;
+            Assert.IsNotNull(registrar);
+            Assert.AreSame(Registrar.GetDefaultInstance(), registrar);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void Dispose_DoesNotChangeObjects()
+        {
+            var dataManager = DefaultActionContext.GetDefaultInstance().DataManager;
+            var selectAction = DefaultActionContext.GetDefaultInstance().SelectAction;
+            var registrar = DefaultActionContext.GetDefaultInstance().Registrar;
+
+            DefaultActionContext.GetDefaultInstance().Dispose();
+
+            Assert.AreSame(dataManager, DefaultActionContext.GetDefaultInstance().DataManager);
+            Assert.AreSame(selectAction, DefaultActionContext.GetDefaultInstance().SelectAction);
+            Assert.AreSame(registrar, DefaultActionContext.GetDefaultInstance().Registrar);
+        }
+    }
+}
---
+++ src/ActionsTests/Contexts/ScopedActionContextTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Axe.Windows.Actions;
+using Axe.Windows.Actions.Contexts;
+using Axe.Windows.Desktop.UIAutomation.CustomObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Axe.Windows.ActionsTests.Contexts
+{
+    [TestClass]
+    public class ScopedActionContextTests
+    {
+        [TestMethod]
+        [Timeout(1000)]
+        public void DataManager_IsnotDefaultDataManager()
+        {
+            using (var actionContext = ScopedActionContext.CreateInstance())
+            {
+                Assert.IsNotNull(actionContext.DataManager);
+                Assert.AreNotSame(actionContext.DataManager, DataManager.GetDefaultInstance());
+            }
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void SelectAction_IsNotDefaultSelectAction()
+        {
+            using (var actionContext = ScopedActionContext.CreateInstance())
+            {
+                Assert.IsNotNull(actionContext.SelectAction);
+                Assert.AreNotSame(actionContext.SelectAction, SelectAction.GetDefaultInstance());
+            }
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void Registrar_IsNotDefaultSelectAction()
+        {
+            using (var actionContext = ScopedActionContext.CreateInstance())
+            {
+                Assert.IsNotNull(actionContext.Registrar);
+                Assert.AreNotSame(actionContext.Registrar, Registrar.GetDefaultInstance());
+            }
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void CreateInstance_DifferentObjectsOnEachCall()
+        {
+            using (var actionContext1 = ScopedActionContext.CreateInstance())
+            {
+                using (var actionContext2 = ScopedActionContext.CreateInstance())
+                {
+                    AssertAreDifferentObjectsOfType<ScopedActionContext>(actionContext1, actionContext2);
+                    AssertAreDifferentObjectsOfType<DataManager>(actionContext1.DataManager, actionContext2.DataManager);
+                    AssertAreDifferentObjectsOfType<SelectAction>(actionContext1.SelectAction, actionContext2.SelectAction);
+                    AssertAreDifferentObjectsOfType<Registrar>(actionContext1.Registrar, actionContext2.Registrar);
+                }
+            }
+        }
+
+        private void AssertAreDifferentObjectsOfType<T>(object object1, object object2)
+        {
+            Assert.IsInstanceOfType(object1, typeof(T));
+            Assert.IsInstanceOfType(object2, typeof(T));
+
+            Assert.IsNotNull(object1);
+            Assert.IsNotNull(object2);
+            Assert.AreNotSame(object1, object2);
+        }
+    }
+}
