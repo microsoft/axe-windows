59edb8f4c954ed92a1e5d24fa533af77ccf890c8
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-10-06 17:42:04 -0700

    Remove unnecessary using blocks

--- src/AutomationTests/SnapshotCommandTests.cs
+++ src/AutomationTests/SnapshotCommandTests.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-using Axe.Windows.Actions;
+
 using Axe.Windows.Actions.Contexts;
 using Axe.Windows.Automation;
 using Axe.Windows.Core.Bases;
