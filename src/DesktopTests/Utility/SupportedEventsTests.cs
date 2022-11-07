b0c2af21e4e38b30858811744cd5f5e29d0e045c
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-11-07 10:59:37 -0800

    chore: Provide consistent spacing after license header

--- src/DesktopTests/Utility/SupportedEventsTests.cs
+++ src/DesktopTests/Utility/SupportedEventsTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Axe.Windows.Core.Types;
 using Axe.Windows.Desktop.Types;
 using Axe.Windows.Desktop.Utility;
