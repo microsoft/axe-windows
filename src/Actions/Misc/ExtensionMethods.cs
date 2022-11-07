6595535cca00eb9c67c50bc6c8bbda2e89ac5311
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-11-07 11:01:02 -0800

    chore: Provide consistent spacing after license header

--- src/Actions/Misc/ExtensionMethods.cs
+++ src/Actions/Misc/ExtensionMethods.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Axe.Windows.Actions.Contexts;
 using Axe.Windows.Actions.Resources;
 using Axe.Windows.Core.Bases;
--- src/AutomationTests/AutomationIntegrationTests.cs
+++ src/AutomationTests/AutomationIntegrationTests.cs
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using Axe.Windows.Automation;
 using Axe.Windows.Automation.Data;
 using Axe.Windows.UnitTestSharedLibrary;
