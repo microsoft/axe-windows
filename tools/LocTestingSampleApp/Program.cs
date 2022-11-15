738b9c5baac876d00c75e31eec105a21c5203513
sfoslund <sfoslund@microsoft.com>
2022-11-15 15:39:35 -0800

    Add license header

--- tools/LocTestingSampleApp/Program.cs
+++ tools/LocTestingSampleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using Axe.Windows.Automation;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Axe.Windows.Automation;
 using Axe.Windows.Automation.Data;
 using Newtonsoft.Json;
 using System.Diagnostics;
