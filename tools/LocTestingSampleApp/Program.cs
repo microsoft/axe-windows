7641209e3d0f13bf6b1b92d829671f3d1f7600c8
Sarah Oslund <sfoslund@microsoft.com>
2022-11-16 09:28:31 -0800

    chore: Add localization testing tool to tools directory (#844)
    #### Details
    
    Adds a test app that can be used to test axe-windows localization to the
    tools directory. See the README for details on usage.
    
    ##### Motivation
    
    Keep all tools related to axe-windows in one place
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a"
    in the checkbox -->
    - [n/a] Addresses an existing issue: #0000
    
    Co-authored-by: Dave Tryon <45672944+DaveTryon@users.noreply.github.com>

---
+++ tools/LocTestingSampleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Axe.Windows.Automation;
+using Axe.Windows.Automation.Data;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace AxeWinLocTesting
+{
+    internal class Program
+    {
+        // Change this code to experiment with differnet languages, for example "es", "it", "ja", "ko".
+        private static string langCode = "fr";
+
+        // Change this path to experiment with different test apps.
+        private static string exePath = "../WildlifeManager/WildlifeManager.exe";
+
+        // True will show full JSON scan results, false will just show a short example. The long output can
+        // be very long and confusing, since it serializes both content that should be localized (ex: rule
+        // descriptions/ text) and content that should not be localized (ex: property/field names).
+        private static bool showFullResults = false;
+
+        static void Main(string[] _)
+        {
+            var process = StartTestExe();
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(langCode);
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(langCode);
+            try
+            {
+                var scanner = CreateScanner(process.Id);
+
+                var scanResults = GetScanResults(scanner);
+
+                PrintScanResults(scanResults, showFullResults);
+            }
+            finally
+            {
+                process.Kill();
+            }
+        }
+
+        private static Process StartTestExe()
+        {
+            var process = Process.Start(exePath);
+            process.WaitForInputIdle();
+            return process;
+        }
+
+        private static IScanner CreateScanner(int processId)
+        {
+            Console.WriteLine("Creating scanner...");
+            var configBuilder = Config.Builder.ForProcessId(processId)
+                .WithOutputFileFormat(OutputFileFormat.None);
+            var config = configBuilder.Build();
+            var scanner = ScannerFactory.CreateScanner(config);
+            return scanner;
+        }
+
+        private static ScanOutput GetScanResults(IScanner scanner)
+        {
+            Console.WriteLine("Scanning...");
+            var scanResults = scanner.Scan(null);
+            return scanResults;
+        }
+
+        private static void PrintScanResults(ScanOutput scanResults, bool fullContent)
+        {
+            Console.WriteLine("Scan results:");
+            Console.WriteLine();
+            Console.WriteLine("===========================================");
+            if (fullContent)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(scanResults, Formatting.Indented));
+            }
+            else
+            {
+                foreach (var scanResult in scanResults.WindowScanOutputs)
+                {
+                    foreach (var error in scanResult.Errors)
+                    {
+                        Console.WriteLine(error.Rule.Description);
+                    }
+                }
+            }
+            Console.WriteLine("===========================================");
+            Console.WriteLine();
+        }
+    }
+}
