f070393a549ec758f16ca84ce5068852f05c4b70
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2023-07-19 18:50:33 -0700

    Leverage existing Condtion

--- src/Rules/Library/ChromiumComponentsShouldUseWebScanner.cs
+++ src/Rules/Library/ChromiumComponentsShouldUseWebScanner.cs
@@ -4,8 +4,7 @@
 using Axe.Windows.Core.Bases;
 using Axe.Windows.Core.Enums;
 using Axe.Windows.Rules.Resources;
-using static Axe.Windows.Rules.PropertyConditions.ControlType;
-using static Axe.Windows.Rules.PropertyConditions.Framework;
+using static Axe.Windows.Rules.PropertyConditions.ElementGroups;
 
 namespace Axe.Windows.Rules.Library
 {
@@ -26,7 +25,7 @@ namespace Axe.Windows.Rules.Library
 
         protected override Condition CreateCondition()
         {
-            return Chrome & Document;
+            return IsChromiumDocument;
         }
     } // class
 } // namespace
