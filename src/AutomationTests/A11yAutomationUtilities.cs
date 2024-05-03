430ac50ac88d5606c226141ee60ce468c045ec1e
Forrest Dillaway <forrestd@microsoft.com>
2024-05-03 11:00:38 -0700

    Implement GetDepthFirstLastLeafHWNDElement to ensure we find a control that has nonzero HWND for scoped UI scan testing.

--- src/AutomationTests/A11yAutomationUtilities.cs
+++ src/AutomationTests/A11yAutomationUtilities.cs
@@ -3,6 +3,10 @@
 
 using Axe.Windows.Core.Enums;
 using Axe.Windows.Desktop.UIAutomation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using UIAutomationClient;
 
@@ -13,6 +17,13 @@ namespace Axe.Windows.AutomationTests
     /// </summary>
     public class A11yAutomationUtilities
     {
+        internal static DesktopElement GetRootElement()
+        {
+            IUIAutomation uiAutomation = A11yAutomation.GetDefaultInstance().UIAutomation;
+            IUIAutomationElement focusedElement = uiAutomation.GetRootElement();
+            return new DesktopElement(focusedElement, keepElement: true, setMembers: true);
+        }
+
         internal static DesktopElement GetFocusedElement()
         {
             IUIAutomation uiAutomation = A11yAutomation.GetDefaultInstance().UIAutomation;
@@ -20,17 +31,13 @@ namespace Axe.Windows.AutomationTests
             return new DesktopElement(focusedElement, keepElement: true, setMembers: true);
         }
 
-        internal static DesktopElement GetDepthFirstLastLeafControlElement(DesktopElement rootElement)
+        internal static DesktopElement GetDepthFirstLastLeafHWNDElement(DesktopElement rootElement)
         {
             var walker = A11yAutomation.GetDefaultInstance().GetTreeWalker(TreeViewMode.Control);
             try
             {
-                IUIAutomationElement leafElement = (IUIAutomationElement)rootElement.PlatformObject;
-                for (IUIAutomationElement currentElement = walker.GetLastChildElement(leafElement); currentElement != null; currentElement = walker.GetLastChildElement(leafElement))
-                {
-                    Marshal.ReleaseComObject(leafElement);
-                    leafElement = currentElement;
-                }
+                IUIAutomationElement rootAutomationElement = rootElement.PlatformObject;
+                IUIAutomationElement leafElement = FindLastWindowHandleElement(rootAutomationElement);
 
                 return leafElement is null
                     ? rootElement
@@ -39,6 +46,52 @@ namespace Axe.Windows.AutomationTests
             finally
             {
                 Marshal.ReleaseComObject(walker);
+            }
+
+            IUIAutomationElement FindLastWindowHandleElement(IUIAutomationElement parent)
+            {
+                List<IUIAutomationElement> matchingElements = new();
+                for (var child = walker.GetFirstChildElement(parent); child != null; child = MatchAndMoveNext(child))
+                {
+                }
+
+                foreach (var element in matchingElements)
+                {
+                    FindLastWindowHandleElement(element);
+                }
+
+                foreach (var element in matchingElements.Take(matchingElements.Count - 1))
+                {
+                    Marshal.ReleaseComObject(element);
+                }
+
+                return matchingElements.LastOrDefault();
+
+                IUIAutomationElement MatchAndMoveNext(IUIAutomationElement element)
+                {
+                    IUIAutomationElement next;
+                    try
+                    {
+                        next = walker.GetNextSiblingElement(element);
+                    }
+                    catch(COMException e)
+                    {
+                        Debug.WriteLine(message: $"Error getting sibling of {element.CurrentName}: {e}");
+                        next = null;
+                    }
+
+                    if (element.CurrentNativeWindowHandle != IntPtr.Zero)
+                    {
+                        Debug.WriteLine(message: $"HWND={element.CurrentNativeWindowHandle}; Name='{element.CurrentName}'; ClassName='{element.CurrentClassName}'");
+                        matchingElements.Add(element);
+                    }
+                    else
+                    {
+                        Marshal.ReleaseComObject(element);
+                    }
+
+                    return next;
+                }
             }
         }
     }
