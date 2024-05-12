66373f3f669fdee9380c3d5d56dbc088714da35a
Bill Dengler <codeofdusk@gmail.com>
2024-05-11 19:45:01 -0700

    Move using

--- src/Automation/Interfaces/IScanTools.cs
+++ src/Automation/Interfaces/IScanTools.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Axe.Windows.Automation
 {
-    using System;
-
     /// <summary>
     /// Encapsulates the set of tools used to scan, assemble results, and write output files
     /// </summary>
